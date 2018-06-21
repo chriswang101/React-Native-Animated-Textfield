b79439fdb26a5cd1cd15af80c1756882d2c64976
Chris Wang <cwang@safebutler.com>
2018-06-21 14:30:12 -0700

    fix indentation

--- example/App.js
+++ example/App.js
@@ -1,58 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    View,
+    TouchableOpacity,
+    Text,
+    LayoutAnimation,
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import TextField from '../module/TextField'
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            shouldShowSecondaryColor: false,
+        }
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    render() {
+        return (
+            <View>
+                <TextField
+                    label={'Label'}
+                    placeholder={'Tap Here First'}
+                    accentColor={'deepskyblue'}
+                    secondaryAccentColor={'magenta'}
+                    shouldShowSecondaryColor={this.state.shouldShowSecondaryColor}
+                    fontFamily={'Helvetica Neue'}
+                    floatingTextFontWeight={'500'}
+                    fontSize={24}
+                    animationDuration={250}
+                    selectionColor={'mediumslateblue'}
+                    underlineWeight={2}
+                    onTextFieldFocus={() => console.log('onFocus')}
+                    onTextFieldBlur={() => console.log('onBlur')}
+                    onChangeTextFieldText={(text) => console.log(text)}
+                    viewStyle={{
+                        marginTop: 60,
+                        marginHorizontal: 10,
+                    }}
+                />
+
+                <TouchableOpacity
+                    onPress={() => {
+                        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+                        this.setState({
+                            shouldShowSecondaryColor: !this.state.shouldShowSecondaryColor,
+                        });
+
+                    }}
+                    style={{
+                        borderRadius: 5,
+                        backgroundColor: 'mediumslateblue',
+                        marginVertical: 20,
+                        marginHorizontal: 10,
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: 'white',
+                            fontSize: 32,
+                            fontWeight: '600',
+                            paddingVertical: 10,
+                            alignSelf: 'center',
+                        }}
+
+                    >
+                        Then Press Here
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
